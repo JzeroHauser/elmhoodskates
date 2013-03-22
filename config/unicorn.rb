ab33e8e5dc71239bddcbfce9fc918db57fa89653
Jzero Hauser <jzero.hauser@gmail.com>
2013-03-21 21:45:19 -0500

    some server setup

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/srv/jzero/elmhoodskates"
+pid "/srv/jzero/elmhoodskates/tmp/pids/unicorn.pid"
+stderr_path "/srv/jzero/elmhoodskates/log/unicorn.log"
+stdout_path "/srv/jzero/elmhoodskates/log/unicorn.log"
+
+listen "/tmp/unicorn.todo.sock"
+worker_processes 1
+timeout 30
