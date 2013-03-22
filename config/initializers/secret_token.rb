71df96ed8ac1671cabd9c03895d2e66f6a54a0cb
Jzero Hauser <jhauser40@academic.rrc.ca>
2013-03-21 19:06:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elmhoodskates::Application.config.secret_token = '049880dc36d038351b28e9af43b0ceb9e1ca72b0581716ca177d84ae1e5c224565bfa4decd4a7e857f7579e632c9b9ffee716e0e3805239190d4d1948c40e00d'
