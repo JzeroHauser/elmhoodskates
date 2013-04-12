ce91d14efa13e292b73d1925f380053f2d4e61fe
Jzero Hauser <jzero.hauser@gmail.com>
2013-04-11 20:14:13 -0500

    messed around with the cart controller

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,7 +1,7 @@
 class OrderController < ApplicationController
   def purchase
     session[:shopping_cart] ||= Shopping_cart.new
-    session[:shopping_cart].add_good params[:id]
+    session[:shopping_cart].add_good params[:id], params[:q]
     redirect_to :back
   end
 end
--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,24 +1,22 @@
 class Shopping_cart
 
-  attr_accessor :id, :quantity, :goods, :good
+  attr_accessor :goods
 
   def initialize
-    @id = id
-    @quantity = quantity
     @goods = []
   end
 
-  def add_good id
+  def add_good id, quantity
     good = []
     good[0] = id
-    good[1] = 1
-    @goods << good unless existing_good?(good[0])
+    good[1] = quantity.to_i
+    @goods << good unless existing_good?(good[0], good[1])
   end
 
-  def existing_good? id
+  def existing_good? id, quantity
     @goods.each do |g|
       if g[0] == id
-        g[1] += 1
+        g[1] += quantity.to_i
         return true
       end
     end
